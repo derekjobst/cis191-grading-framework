40ffe7bd9003136e6f9e4b486ee8ec876dc95a6f
Satya Bodduluri <god@pineapple.(none)>
2014-03-07 14:24:56 -0500

    finished archiver

---
+++ archiver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+file="191_sample.csv"
+
+name=${2}_${1}_backup
+
+tar -cvf ${name}.tar ./191_sample.csv
+
+while IFS=", " read line
+do
+	IFS=", " read -a array <<< "$line"
+	if [ ${array[0]} != "PENNKEY" ]
+	then
+		for i in $(find -name "${array[0]}*"); 
+		do
+			tmp=`sed "s/\n/ /g" <<< $i`
+			tar -rvf ${name}.tar $tmp
+		done
+	fi
+done < $file
+
+tar -czf ${name}.tar.gz ${name}.tar
+rm -rf ${name}.tar
